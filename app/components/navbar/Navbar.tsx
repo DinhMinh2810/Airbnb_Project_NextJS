5267c2517bc5b85b14704f4266c6c7c00006b9a1
dinh_minh <dinhminh28102000@gmail.com>
2023-05-24 13:32:11 +0700

    Create NextJS app

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="text-center sm:text-left fixed w-full bg-white z-10 shadow-sm ">
+      sds
+      <ul>
+        <li>One</li>
+        <li>Two</li>
+        <li>Three</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
