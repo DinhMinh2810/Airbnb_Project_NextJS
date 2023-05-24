7be5f0a629d11503a9d11500e945f5f018a00344
dinh_minh <dinhminh28102000@gmail.com>
2023-05-24 14:11:07 +0700

    Make Navbar UI

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,14 +1,36 @@
 import React from "react";
+import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
   return (
-    <div className="text-center sm:text-left fixed w-full bg-white z-10 shadow-sm ">
-      sds
-      <ul>
-        <li>One</li>
-        <li>Two</li>
-        <li>Three</li>
-      </ul>
+    <div className="fixed w-full bg-white z-10 shadow-sm ">
+      <div
+        className="
+          py-4 
+          border-b-[1px]
+        "
+      >
+        <Container>
+          <div
+            className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          "
+          >
+            <Logo />
+            <Search />
+            <UserMenu />
+            
+          </div>
+        </Container>
+      </div>
     </div>
   );
 };
